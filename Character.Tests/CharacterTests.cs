b04ab6ef6a121e12df50f5ca09bb2062acad309d
bianca gianina <biagianina02@gmail.com>
2019-08-19 14:52:21 +0300

    implemented class character

---
+++ Character.Tests/CharacterTests.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+
+namespace Range.Tests
+{
+    public class CharacterTests
+    {
+        [Fact]
+        public void ReturnsFalseForEmptyString()
+        {
+            var pattern = new Character('a');
+            Assert.False(pattern.Match(""));
+        }
+
+        [Fact]
+        public void ReturnsTrueForCorrectString()
+        {
+            var pattern = new Character('a');
+            Assert.True(pattern.Match("abc"));
+        }
+
+        [Fact]
+        public void ReturnsFalseForCorrectString()
+        {
+            var pattern = new Character('a');
+            Assert.False(pattern.Match("bac"));
+        }
+    }
+}
