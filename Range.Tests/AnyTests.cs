976c8b6da9de5f4cb95e65110ca1ae4647796c52
bianca gianina <biagianina02@gmail.com>
2019-08-20 16:49:16 +0300

    Implemented class any

---
+++ Range.Tests/AnyTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xunit;
+
+namespace Classes.Tests
+{
+    public class AnyTests
+    {
+        [Fact]
+        public void ReturnsFalseAndEmptyForEmptyString()
+        {
+            var e = new Any("eE");
+            Assert.False(e.Match("").Succes());
+            Assert.Equal("", e.Match("").RemainingText());
+        }
+
+        [Fact]
+        public void ReturnsTrueAndRemainingForShortString()
+        {
+            var e = new Any("eE");
+            Assert.True(e.Match("ea").Succes());
+            Assert.Equal("a", e.Match("ea").RemainingText());
+        }
+
+        [Fact]
+        public void ReturnsFalseAndTextForShortString()
+        {
+            var e = new Any("eE");
+            Assert.False(e.Match("a").Succes());
+            Assert.Equal("a", e.Match("a").RemainingText());
+        }
+
+        [Fact]
+        public void ReturnsCorrectStringForSignString()
+        {
+            var sign = new Any("-+");
+            Assert.True(sign.Match("+3").Succes());
+            Assert.Equal("3", sign.Match("+3").RemainingText());
+        }
+
+        [Fact]
+        public void ReturnsCorrectForIncorrectSignString()
+        {
+            var sign = new Any("-+");
+            Assert.False(sign.Match("2").Succes());
+            Assert.Equal("2", sign.Match("2").RemainingText());
+        }
+    }
+}
---
+++ Range/Any.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    public class Any 
+    {
+        readonly string accepted;
+
+        public Any(string accepted)
+        {
+            this.accepted = accepted;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return new Match(false, text);
+            }
+
+            if (accepted.Contains(text[0]))
+            {
+                return new Match(true, text.Substring(1));
+            }
+
+            return new Match(false, text);
+        }
+    }
+}
