40488f441cad172aa4e0adec2dbf381bc2e461e8
bianca gianina <biagianina02@gmail.com>
2019-08-21 13:28:03 +0300

    Implemented OneOrMoreClass

---
+++ Range.Tests/OneOrMoreTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xunit;
+
+namespace Classes.Tests
+{
+    public class OneOrMoreTests
+    {
+        [Fact]
+        public void ReturnsFalseAndEmptyForEmptyString()
+        {
+            var a = new OneOrMore(
+                new Range('0', '9'));
+            Assert.False(a.Match("").Succes());
+            Assert.Equal("", a.Match("").RemainingText());
+        }
+
+        [Fact]
+        public void ReturnsTrueAndEmptyForThreeMatchesString()
+        {
+            var a = new OneOrMore(
+                new Range('0', '9'));
+            Assert.True(a.Match("123").Succes());
+            Assert.Equal("", a.Match("123").RemainingText());
+        }
+
+        [Fact]
+        public void ReturnsTrueAndRemainingForTwoMatchesString()
+        {
+            var a = new OneOrMore(
+                new Range('0', '9'));
+            Assert.True(a.Match("12a").Succes());
+            Assert.Equal("a", a.Match("12a").RemainingText());
+        }
+
+        [Fact]
+        public void ReturnsFalseAndTextForNoMatchesString()
+        {
+            var a = new OneOrMore(
+                new Range('0', '9'));
+            Assert.False(a.Match("bc").Succes());
+            Assert.Equal("bc", a.Match("bc").RemainingText());
+        }
+
+
+    }
+}
---
+++ Range/OneOrMore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    public class OneOrMore
+    {
+        readonly IPattern pattern;
+        readonly Many many;
+
+        public OneOrMore(IPattern pattern)
+        {
+            this.pattern = pattern;
+            this.many = new Many(pattern);
+        }
+
+        public IMatch Match(string text)
+        {
+            var match = pattern.Match(text);
+            if(match.Succes())
+            {
+                return many.Match(match.RemainingText());
+            }
+
+            return new Match(false, text);
+        }
+    }
+}
